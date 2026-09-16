128badb317f02baaeff1a269a6efffa07541f377
navdeep bhardwaj <navdeep.bhardwaj309@gmail.com>
2026-09-16 21:30:02 +0530

    product of array

--- myprogressapp1/Algorithms/prefixSufix/ProductOfArray.cs
+++ myprogressapp1/Algorithms/prefixSufix/ProductOfArray.cs
@@ -8,7 +8,22 @@ int[] arr={1,2,3,4};
 
 int[] ProductOfArray(int[] arr)
 {
-    
+    int [] result=new int[arr.Length] ;
+    result[0]=1;
+    for(int i=1;i<arr.Length; i++)
+    {
+        result[i]=result[i-1]*arr[i-1];
+    } 
+    //result={1,1,2,6}
+    int sufffix=1;
+    for(int i=arr.Length-1; i>0;i--)
+    {
+        result[i]=result[i]*sufffix;
+        sufffix*=arr[i];
+
+    }
+return result; 
+
 }
 
 int[] result=ProductOfArray(arr);
